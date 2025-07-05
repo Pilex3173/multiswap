14d2cc514d91de402b4678d0812c2c4c39606473
Pilex3173 <158312970+Pilex3173@users.noreply.github.com>
2025-07-05 13:09:56 +0700

    Add files via upload

---
+++ src/chains/chains.ts
@@ -0,0 +1,43 @@
+
+export const CHAINS = {
+  helios: {
+    chainId: 42000,
+    chainName: "Helios Testnet",
+    rpc: "https://testnet1.helioschainlabs.org/",
+    currency: {
+      name: "HELIOS",
+      symbol: "tHEL",
+      decimals: 18,
+    },
+  },
+  lumera: {
+    chainId: 9901,
+    chainName: "Lumera Testnet",
+    rpc: "https://api-t.lumera.nodestake.org",
+    currency: {
+      name: "Lumera",
+      symbol: "tLUM",
+      decimals: 6,
+    },
+  },
+  empe: {
+    chainId: 9902,
+    chainName: "EMPE Testnet",
+    rpc: "https://lcd-testnet.empe.io",
+    currency: {
+      name: "Empe",
+      symbol: "tEMP",
+      decimals: 6,
+    },
+  },
+  xos: {
+    chainId: 9903,
+    chainName: "XOS Testnet",
+    rpc: "https://api-t.xos.nodestake.org",
+    currency: {
+      name: "XOS",
+      symbol: "tXOS",
+      decimals: 6,
+    },
+  }
+}
