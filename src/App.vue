22e92e986fceeaf702a8cf845ea82810401b5575
Pilex3173 <158312970+Pilex3173@users.noreply.github.com>
2025-07-05 13:05:06 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <div class="p-4 font-sans">
+    <h1 class="text-2xl font-bold mb-4">Multichain Swap (Testnet)</h1>
+    <wallet-connect />
+  </div>
+</template>
+
+<script setup lang="ts">
+import WalletConnect from './components/WalletConnect.vue'
+</script>
